8060e8129e2a273a09076a264fcdb8448c3531bd
bohdantyshchenko-prog <bohdantyshchenko@gmail.com>
2026-07-30 23:25:39 +0300

    Cache NOVIQ 1.1 compatibility layer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='noviq-v1.1-2026-07-30';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const ASSETS=['./','./index.html','./styles.css','./compat.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
