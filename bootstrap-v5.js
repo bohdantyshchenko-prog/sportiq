b88dce42c3d5facec7438dd177f0676bcc35c460
bohdantyshchenko-prog <bohdantyshchenko@gmail.com>
2026-08-03 19:19:13 +0300

    Add NOVIQ 5 state migration bootstrap

---
+++ bootstrap-v5.js
@@ -0,0 +1,1 @@
+(() => { const N=window.NOVIQ; N.config.version='5.0.0'; N.config.storageKey='noviq-v5-state'; N.config.legacyKeys=['noviq-v4.1-state',...(N.config.legacyKeys||[])]; N.config.buildDate='2026-08-03'; N.defaultState.version='5.0.0'; N.defaultState.schemaVersion=5; N.defaultState.onboarding={completed:false,completedAt:null}; })();
