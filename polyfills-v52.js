3f7c669efc5747cfcb6b438e52e2ae3946fe13a5
bohdantyshchenko-prog <bohdantyshchenko@gmail.com>
2026-08-04 12:25:50 +0300

    add explicit browser compatibility polyfills

---
+++ polyfills-v52.js
@@ -0,0 +1,9 @@
+(() => {
+  'use strict';
+  if (typeof globalThis.structuredClone !== 'function') {
+    globalThis.structuredClone = value => JSON.parse(JSON.stringify(value));
+  }
+  if (!Array.prototype.at) {
+    Object.defineProperty(Array.prototype, 'at', { value(index) { const i=index<0?this.length+index:index; return this[i]; }, configurable:true, writable:true });
+  }
+})();
