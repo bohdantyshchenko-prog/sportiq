862b3b3e7a8a4e879f0d0087fd85ce32e4d5cc28
bohdantyshchenko-prog <bohdantyshchenko@gmail.com>
2026-08-04 12:27:31 +0300

    cache all active NOVIQ 5.2 runtime modules

--- sw-v52.js
+++ sw-v52.js
@@ -1,6 +1,6 @@
 const VERSION='5.2.0';
 const CACHE=`noviq-${VERSION}`;
-const CORE=['./','./index.html','./styles-v52.css','./data.js','./core-v52.js','./app-v52.js','./manifest.webmanifest','./icon.svg'];
+const CORE=['./','./index.html','./styles-v52.css','./polyfills-v52.js','./data.js','./core-v52.js','./app-v52.js','./a11y-v52.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil((async()=>{const cache=await caches.open(CACHE);for(const asset of CORE){try{await cache.add(asset)}catch(error){if(['./','./index.html','./core-v52.js','./app-v52.js','./styles-v52.css'].includes(asset))throw error;console.warn('Optional asset failed',asset)}}await self.skipWaiting()})()));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
 self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
